cd83ea264ce18f569c3d2199625036b443c03ec2
v8phan <56612604+v8phan@users.noreply.github.com>
2021-10-21 20:43:03 -0500

    pagination

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
-import Header from './components/ui/Header/Header.js'
-import CocktailGrid from './components/cocktails/CocktailGrid'
-import Search from '../src/components/ui/Search/Search'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Header from "./components/ui/Header/Header.js";
+import CocktailGrid from "./components/cocktails/CocktailGrid";
+import Search from "../src/components/ui/Search/Search";
+import Pagination from "./components/ui/Pagination/Pagination";
 
 const App = () => {
   //items defines piece of state, characters that come from api; setItems is function to manipulate the state, useState is an empty array by default
   //loading is true by default, false after it finishes
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [query, setQuery] = useState('')
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  const [letter, getLetter] = useState("");
 
-  useEffect (() => {
+  useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+      const result = await axios(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`
+      );
 
-
-//https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-
-      setItems(result.data.drinks)
-      setIsLoading(false)
+      setItems(result.data.drinks);
+      setIsLoading(false);
       //console.log(result.data.drinks)
-    }
-//query is a dependency, fires off useEffect whenever it changes
-    fetchItems()
-  }, [query])
+    };
+    fetchItems();
+  }, [letter]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`
+      );
+
+      //https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+      setItems(result.data.drinks);
+      setIsLoading(false);
+      //console.log(result.data.drinks)
+    };
+    //query is a dependency, fires off useEffect whenever it changes
+    fetchItems();
+  }, [query]);
+
+  const paginate = letter => getLetter(letter);
 
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
-      <Search getQuery={(q) => setQuery(q)}/>
-      <CocktailGrid isLoading={isLoading} items={items}/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CocktailGrid isLoading={isLoading} items={items} />
+      <Pagination paginate={paginate}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ui/Pagination/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Pagination.css";
+
+
+const Pagination = ({ paginate }) => {
+
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {alphabet.map(letter => (
+                    <li key={letter} className='page-item'> 
+                        <a onClick={() => paginate(letter)} href='!#' className='page-link'>
+                        {letter}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
